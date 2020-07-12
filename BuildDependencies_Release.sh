73fe90632ca63e9ed5f9d37684b547b4d122c590
Macoy Madson <macoymadson@gmail.com>
2020-07-12 10:42:10 -0700

    Cleaned up build a bit

--- BuildDependencies_Release.sh
+++ BuildDependencies_Release.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo Building Base2.0...
+echo "Building Base2.0 (and by extension, SFML)..."
 # TODO: Fix this foolishness
 cd libs/base2.0
 ./BuildDependencies_Release.sh || exit 1
 jam -j4 && jam -j4 libBase20 || exit 1
 cd ../../
-echo Finished building Base2.0!
+echo "Finished building Base2.0!"
